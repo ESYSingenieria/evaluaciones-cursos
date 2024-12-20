20d000fa2595ed22d71bbbeabee946b0748a5635
ESYSingenieria <fduarte@esys.cl>
2024-12-20 03:58:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ const loadEvaluation = async () => {
         let attempts = 0;
         if (!snapshot.empty) {
             attempts = snapshot.docs[0].data().attempts || 0;
-            if (attempts >= 1.5) {
+            if (attempts >= 2) {
                 alert('Has alcanzado el número máximo de intentos para esta evaluación.');
                 window.location.href = 'dashboard.html';
                 return;
